5a0656e4b360bc19a3c59288f17ff1b73722c1af
AnaKhurs <ana.khurs@gmail.com>
2021-10-10 16:31:24 +0300

    01-02-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    console.log('App rendering')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <AppTitle />
+        <Rating />
+        <Accordion />
     </div>
   );
+}
+
+function Rating() {
+    console.log('Rating rendering')
+    return (
+        <>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </>
+    )
+}
+
+function Accordion() {
+    console.log('Accordion rendering')
+    return (
+        <div>
+           <AccordionTitle />
+            <AccordionBody />
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    console.log('AccordionTitle rendering')
+    return  <h3>Menu</h3>
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+    )
+}
+function Star() {
+    console.log('Star rendering')
+    return (
+        <div>star</div>
+    )
+}
+
+function AppTitle() {
+    console.log('AppTitle rendering')
+    return <>This is APP component</>
 }
 
 export default App;
