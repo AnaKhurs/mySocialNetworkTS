fea0ffa29c2c0b9c3f054585144ae427d5277f7c
AnaKhurs <ana.khurs@gmail.com>
2021-10-10 18:27:33 +0300

    03-props

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,34 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
     console.log('App rendering')
   return (
     <div>
-        <AppTitle />
-        <Rating />
-        <Accordion />
+        <PageTitle title={"This is APP component"}/>
+        <PageTitle title={"My friends"}/>
+        Article 1
+        <Rating value = {3}/>
+        <Accordion title = {"Menu"}/>
+        <Accordion title = {"Two accordion"}/>
+        Article 2
+        <Rating value = {0}/>
+        <Rating value = {1}/>
+        <Rating value = {2}/>
+        <Rating value = {3}/>
+        <Rating value = {4}/>
+        <Rating value = {5}/>
     </div>
   );
 }
 
-function Rating() {
-    console.log('Rating rendering')
-    return (
-        <>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-        </>
-    )
-}
 
-function Accordion() {
-    console.log('Accordion rendering')
-    return (
-        <div>
-           <AccordionTitle />
-            <AccordionBody />
-        </div>
-    )
-}
-
-function AccordionTitle() {
-    console.log('AccordionTitle rendering')
-    return  <h3>Menu</h3>
-}
-
-function AccordionBody() {
-    console.log('AccordionBody rendering')
-    return (
-        <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>
-    )
-}
-function Star() {
-    console.log('Star rendering')
-    return (
-        <div>star</div>
-    )
-}
-
-function AppTitle() {
-    console.log('AppTitle rendering')
-    return <>This is APP component</>
+function PageTitle(props: any){
+    debugger
+    console.log('PageTitle rendering')
+    return <h1>{props.title}</h1>
 }
 
 export default App;
