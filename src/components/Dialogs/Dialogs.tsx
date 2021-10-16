04ac43981495a6d31a61f90febcfdbeb6bbfeeed
AnaKhurs <ana.khurs@gmail.com>
2021-10-16 12:50:16 +0300

    roure exact dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
 
 const Dialogs = () => {
     return (
-        <div className={classes.content}>Dialogs</div>
+        <div className={classes.content}>
+            <div className={classes.dialogs}>
+                <div className={classes.dialog + " " + classes.active}>
+                   <NavLink to='/dialogs/1'>Nastya</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/2'>Alex</NavLink>
+                    </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/3'>Masha</NavLink>
+                    </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/4'>Lena</NavLink>
+                    </div>
+                <div className={classes.dialog}>
+                    <NavLink to='/dialogs/5'>Dima</NavLink>
+                    </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>Yooo</div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import MyPosts from "./MyPosts/MyPosts";
 const Profile = () => {
     return (
         <div>
-            <div>
+            <div className={classes.profileImg}>
                 <img src='https://www.tourdom.ru/upload/iblock/c67/c67d37818296f908f1ba70503667e48c.jpeg'/>
             </div>
             <div>
