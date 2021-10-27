9204e6fb18d78536d56cc531eb03af68e6bd6409
AnaKhurs <ana.khurs@gmail.com>
2021-10-27 23:18:50 +0300

    css profilepage

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -9,9 +9,15 @@ type PostType = {
 const Post: React.FC<PostType> = (props) => {
     return (
         <div className={classes.item}>
-            <img src='https://cdn.pixabay.com/photo/2015/03/03/08/55/portrait-657116__340.jpg'/>
-            {props.message}
-            <div>
+            <div className={classes.avaPost}>
+                <div className={classes.ava}>
+                    <img src='https://cdn.pixabay.com/photo/2015/03/03/08/55/portrait-657116__340.jpg'/>
+                </div>
+                <div className={classes.messagePost}>
+                    {props.message}
+                </div>
+            </div>
+            <div className={classes.likeCounte}>
                 <span>like {props.like}</span>
             </div>
 
