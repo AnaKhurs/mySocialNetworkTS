e2b5865d88ef3357c13663a4fa4f8c8ba9ac4b0b
AnaKhurs <ana.khurs@gmail.com>
2021-10-27 22:31:09 +0300

    31_ref_NewPost

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Ref} from 'react';
 import classes from './MyPosts.module.css'
 import Post from "./Post/Post";
 import {PostType} from "./../../../redux/state";
@@ -11,15 +11,24 @@ const MyPosts: React.FC<ProfileInfoType> = (props) => {
 
     let postsElements = props.posts.map((p) => <Post message={p.message} like={p.like}/>)
 
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
+
+    let addPost = () => {
+        if (newPostElement.current) {
+            let text = newPostElement.current.value;
+            alert(text)
+        }
+    }
+
     return (
         <div className={classes.allPosts}>
             <h3>My Posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={classes.posts}>
