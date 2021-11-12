902c9834d8e758a752ff3eff50a520edf2878cf3
AnaKhurs <ana.khurs@gmail.com>
2021-11-12 22:25:47 +0300

    36_this_store

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, {addNewPost, changeNewPostText, StateType, subscribe} from "./redux/state";
+/*import state, {addNewPost, changeNewPostText, StateType, subscribe} from "./redux/state";*/
+import {StateType, store} from "./redux/state"
 import ReactDOM from "react-dom";
 
 
 export const rerenderEntireTree = (state: StateType) => {
-    ReactDOM.render(<App state={state} addNewPost={addNewPost}
-                         changeNewPostText={changeNewPostText}/>, document.getElementById('root')
+    ReactDOM.render(<App state={store.getState()} addNewPost={store.addNewPost.bind(store)}
+                         changeNewPostText={store.changeNewPostText.bind(store)}/>, document.getElementById('root')
     );
 }
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
-rerenderEntireTree(state);
-
-subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,6 +1,6 @@
-let rerenderEntireTree = (state: StateType) => {
+/*let rerenderEntireTree = (state: StateType) => {
     console.log("rerenderEntireTree")
-}
+}*/
 
 export type DialogType = {
     id: number
@@ -46,7 +46,106 @@ export type FriendType = {
     avatar: string
 }
 
-const state: StateType = {
+export type StoreType = {
+    _state: StateType
+    getState: () => StateType
+    addNewPost: (messagePost: string) => void
+    changeNewPostText: (newPostText: string) => void
+    _callSubscriber: (state: StateType) => void
+    subscribe: (observer: (state: StateType) => void) => void
+}
+
+export const store: StoreType = {
+    _state: {
+        profilePage: {
+            newPostText: "",
+            posts: [
+                {id: 1, message: "Hi, how are you?", like: 18},
+                {id: 2, message: "It is my first post!!!", like: 19},
+                {id: 3, message: "Yoooo", like: 10},
+            ],
+        },
+        dialogPage: {
+            dialogs: [
+                {
+                    id: 1,
+                    name: "Nastya",
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0AfKY4bAlZOoZf1-lx6csbUt1LrtdMeuzLaiYapJPAVaLPdfBQSr1dOy5SRO8vev0BdE&usqp=CAU"
+                },
+                {
+                    id: 2,
+                    name: "Alex",
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH8C8VibswhLf3oI9Y-oEncHluuxKte7xvRw&usqp=CAU"
+                },
+                {
+                    id: 3,
+                    name: "Masha",
+                    avatar: "https://s1.r29static.com/bin/entry/ab2/0,398,2000,2000/x,80/1648676/image.jpg"
+                },
+                {
+                    id: 4,
+                    name: "Dima",
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThjiIep4R49K8xkJr_xQxVOXUvblr-wBlbGA&usqp=CAU"
+                },
+                {
+                    id: 5,
+                    name: "Lena",
+                    avatar: "https://pyxis.nymag.com/v1/imgs/359/91f/8fc4e3abb6c8cc68ca9a3389e4b495165b-24-mr-robot.rsquare.w700.jpg"
+                },
+            ],
+            messagesText: [
+                {id: 1, messageText: "Hi!!!", from: "sender"},
+                {id: 2, messageText: "How are you?", from: "sender"},
+                {id: 3, messageText: "yoooooo", from: "receiver"},
+            ],
+        },
+        sidebar: {
+            friends: [
+                {
+                    id: 1,
+                    name: "Lena",
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0AfKY4bAlZOoZf1-lx6csbUt1LrtdMeuzLaiYapJPAVaLPdfBQSr1dOy5SRO8vev0BdE&usqp=CAU"
+                },
+                {
+                    id: 2,
+                    name: "Alex",
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH8C8VibswhLf3oI9Y-oEncHluuxKte7xvRw&usqp=CAU"
+                },
+                {
+                    id: 3,
+                    name: "Nastya",
+                    avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThjiIep4R49K8xkJr_xQxVOXUvblr-wBlbGA&usqp=CAU"
+                },
+            ]
+        }
+    },
+    getState() {
+        return this._state
+    },
+    addNewPost(messagePost: string) {
+        let newPost = {
+            id: 5, message: messagePost, like: 18
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._callSubscriber(this._state);
+        this._state.profilePage.newPostText = "";
+    },
+    changeNewPostText(newPostText: string) {
+        this._state.profilePage.newPostText = newPostText;
+        this._callSubscriber(this._state);
+    },
+    _callSubscriber(state: StateType) {
+        console.log("rerenderEntireTree")
+    },
+    subscribe(observer: (state: StateType) => void) {
+        this._callSubscriber = observer;
+    }
+
+
+}
+
+
+/*const state: StateType = {
     profilePage: {
         newPostText: "",
         posts: [
@@ -108,27 +207,26 @@ const state: StateType = {
             },
         ]
     }
-}
+}*/
 
 
-export const addNewPost = (messagePost: string) => {
+/*export const addNewPost = (messagePost: string) => {
     let newPost = {
         id: 5, message: messagePost, like: 18
     }
     state.profilePage.posts.push(newPost);
     rerenderEntireTree(state);
     state.profilePage.newPostText = "";
-}
+}*/
 
-export const changeNewPostText = (newPostText: string) => {
+/*export const changeNewPostText = (newPostText: string) => {
     state.profilePage.newPostText = newPostText;
     rerenderEntireTree(state);
+}*/
 
-}
-
-export const subscribe = (observer: (state: StateType) => void) => {
+/*export const subscribe = (observer: (state: StateType) => void) => {
     rerenderEntireTree = observer;
-}
+}*/
 
-
-export default state;
+/*
+export default state;*/
