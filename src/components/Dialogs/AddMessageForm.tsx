bdccbd7bff1f2d217c5900280070bd8be3cad4df
AnaKhurs <ana.khurs@gmail.com>
2022-01-24 22:30:24 +0300

    Update AddMessageForm.tsx

--- src/components/Dialogs/AddMessageForm.tsx
+++ src/components/Dialogs/AddMessageForm.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import {Field, InjectedFormProps, reduxForm} from "redux-form";
 import classes from './Dialogs.module.css';
+import {Textarea} from "../common/FormsControls/FormsControls";
+import {maxLenghtCreator, required} from "../../utils/validators/validators";
 
 export type FormDataType = {
     newMessageBody: string
 }
 
+const maxLenght = maxLenghtCreator(50)
 
 const AddMessageForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
     return (
         <form onSubmit={props.handleSubmit} className={classes.messageSend}>
-            <Field component={"textarea"} name={"newMessageBody"} placeholder={"Enter your message"}
+            <Field component={Textarea}
+                   validate={[required, maxLenght]}
+                   name={"newMessageBody"}
+                   placeholder={"Enter your message"}
                    className={classes.textarea}/>
             <button className={classes.button}>Send++++</button>
         </form>
