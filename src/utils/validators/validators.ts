352ea7dccb1201f0d5ec71643458b0859725df55
AnaKhurs <ana.khurs@gmail.com>
2022-01-29 21:01:43 +0300

    Update validators.ts

--- src/utils/validators/validators.ts
+++ src/utils/validators/validators.ts
@@ -5,7 +5,6 @@ export const required = (value: string) => {
 
 }
 
-
 export const maxLengthCreator = (maxLength: number) => (value: string) => {
     if (value.length > maxLength) return `Max length is ${maxLength} symbols`
     return undefined
