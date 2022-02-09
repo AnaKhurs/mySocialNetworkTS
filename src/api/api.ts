2f707763794e70ce8e313d064ea9203460759a57
AnaKhurs <ana.khurs@gmail.com>
2022-02-09 22:05:29 +0300

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosResponse} from "axios";
 import {UserProfileDataType} from "../redux/profile-reducer";
-import {AuthMeResponseType, DataTypeAuthMe} from "../redux/auth-reducer";
+import {DataTypeAuthMe} from "../redux/auth-reducer";
 
 const instance = axios.create(
     {
@@ -64,42 +64,3 @@ export type ResponseType<D = {}> = {
     messages: Array<string>
     data: D
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-// api
-export const todolistsAPI = {
-    getTodolists() {
-        return instance.get<TodolistType[]>('todo-lists');
-    },
-    createTodolist(title: string) {
-        return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: TodolistType }>>>('todo-lists', {title});
-    },
-    deleteTodolist(id: string) {
-        return instance.delete<ResponseType>(`todo-lists/${id}`);
-    },
-    updateTodolist(id: string, title: string) {
-        return instance.put<{ title: string }, AxiosResponse<ResponseType>>(`todo-lists/${id}`, {title});
-    },
-    getTasks(todolistId: string) {
-        return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`);
-    },
-    deleteTask(todolistId: string, taskId: string) {
-        return instance.delete<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`);
-    },
-    createTask(todolistId: string, title: string) {
-        return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks`, {title});
-    },
-    updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
-        return instance.put<UpdateTaskModelType, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
-    }
-}*/
